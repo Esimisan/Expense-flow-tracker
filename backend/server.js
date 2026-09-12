5a76557863256ac74cb386a2ad46389cfd9c84cd
Esimisan <folabiakra@gmail.com>
2026-09-12 13:49:41 +0100

    Implement user authentication with registration and login

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config(); // this must come before the connection to database so it doest come up as undefined
 connectDB();
@@ -15,6 +16,8 @@ app.use(express.json()); //parse incoming JSON bodies into req.body
 app.get("/", (req, res) => {
   res.send("ExpenseFlow API is running");
 });
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {

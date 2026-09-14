44f8ef9e78c170047df41bc6bfc2ef35a285872e
Esimisan <folabiakra@gmail.com>
2026-09-14 02:24:45 +0100

    API for transactions

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
 import authRoutes from "./routes/authRoutes.js";
+import transactionRoutes from "./routes/transactionRoutes.js";
 
 dotenv.config(); // this must come before the connection to database so it doest come up as undefined
 
@@ -19,7 +20,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
-
+app.use("/api/transactions", transactionRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
